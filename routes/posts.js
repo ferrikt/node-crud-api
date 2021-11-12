9c6f0a3df6ecd3951a4c6011a8557deb4909968b
Natallia Skviarniuk <ns106143@VHAMACLOCALADMIN.modem>
2021-11-13 10:49:43 +1100

    redesing using async

--- routes/posts.js
+++ routes/posts.js
@@ -2,30 +2,29 @@ const express = require('express');
 const router = express.Router();
 const Post  = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send("we are on post");
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    }
+    catch(err) {
+        res.json({ message: err });
+    }
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
 
-    post.save()
-    .then(data => {
-        res.json(date)
-    })
-    .catch(err => {
-        res.json({ message: err })
-    })
-    
-})
-
-router.get('/specific', (req, res) => {
-    res.send("we are on specific");
+    try {
+        const data = await post.save();
+        res.json(data);
+    }
+    catch(err) {
+        res.json({ message: err });
+    }    
 })
 
 module.exports = router;
