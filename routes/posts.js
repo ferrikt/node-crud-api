6aecab8c3cb248c293a60014ac8f2f46e378a467
Natallia Skviarniuk <n.skviarniuk@gmail.com>
2021-11-13 15:17:34 +1100

    simple CRUD api

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post  = require('../models/Post');
 
+// GET BACK ALL THE POSTS
 router.get('/', async (req, res) => {
     try {
         const posts = await Post.find();
@@ -12,6 +13,7 @@ router.get('/', async (req, res) => {
     }
 })
 
+// SUBMITS A POST
 router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
@@ -21,6 +23,43 @@ router.post('/', async (req, res) => {
     try {
         const data = await post.save();
         res.json(data);
+    }
+    catch(err) {
+        res.json({ message: err });
+    }    
+})
+
+// SPECIFIC POST
+router.get('/:postId', async (req, res) => {
+    console.log(req.params.postId);
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }
+    catch(err) {
+        res.json({ message: err });
+    }    
+})
+
+// DELETE A  POST
+router.delete('/:postId', async (req, res) => {
+    console.log(req.params.postId);
+    try {
+        const post = await Post.remove({_id:req.params.postId});
+        res.json(post);
+    }
+    catch(err) {
+        res.json({ message: err });
+    }    
+})
+
+
+// UPDATE A  POST
+router.patch('/:postId', async (req, res) => {
+    console.log(req.params.postId);
+    try {
+        const post = await Post.updateOne({_id:req.params.postId}, {$set: { title: req.body.title }});
+        res.json(post);
     }
     catch(err) {
         res.json({ message: err });
